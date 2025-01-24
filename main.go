a05901b906acc892a8aedf8267fec9e1eda24381
Francisco-Teixeirax86 <franjateixeira@gmail.com>
2025-01-24 18:36:57 +0000

    Read the number of tables present
    Read the number of tables present by sqlite_schema page header, contains a 2-byte big-endian value that specifies number of cells on the page.

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 			log.Fatal(err)
 		}
 
-		header := make([]byte, 100)
+		header := make([]byte, 100) //first 100 bytes are reserved for the header
 		_, err = databaseFile.Read(header)
 		if err != nil {
 			log.Fatal(err)
@@ -31,8 +31,20 @@ func main() {
 			return
 		}
 
-		fmt.Println("PageSize: %v", pageSize)
+		leaf := make([]byte, pageSize-100)
+		_, err = databaseFile.Read(leaf)
+		if err != nil {
+			log.Fatal(err)
+		}
 
+		var numTables uint16
+		if err = binary.Read(bytes.NewBuffer(leaf[3:5]), binary.BigEndian, &numTables); err != nil {
+			fmt.Println("Failed to read integer:", err)
+			return
+		}
+
+		fmt.Println("PageSize: ", pageSize)
+		fmt.Println("NumTables: ", numTables)
 	default:
 		fmt.Println("Unsupported command:", command)
 		os.Exit(1)
